999f77851aa88791d60a9f62a8c508cfa67d83c2
Haritha Sabbisetty <104927752+snowflaketrail30@users.noreply.github.com>
2022-05-05 17:49:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit
+import streamlit as st
 import pandas as py
 import requests
 import snowflake.connector as sf
@@ -9,42 +9,44 @@ def get_fruityvice_data(this_fruit_choice):
     #beautify reponse
   fruityvice_normalized = py.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
+
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
   
-streamlit.title('My Mom\'s New Healthy Diner')
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.title('My Mom\'s New Healthy Diner')
+st.header('Breakfast Favorites')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = py.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # put pick list to select
-fruits_selected = streamlit.multiselect("Pick some fruits :", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = st.multiselect("Pick some fruits :", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_show = my_fruit_list.loc[fruits_selected]
 # display the table on tha page
-streamlit.dataframe(fruits_show)
-streamlit.header('Fruityvice Fruit Advice!')
+st.dataframe(fruits_show)
+st.header('Fruityvice Fruit Advice!')
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+  fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
   if not fruit_choice:
-    streamlit.error('Please select a fruit to get information.')
+    st.error('Please select a fruit to get information.')
   else:
     fruityvice_normalized = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(fruityvice_normalized)
+    st.dataframe(fruityvice_normalized)
 except URLError as e:
-  streamlit.error()
+  st.error()
   
+if sf.button('Get Fruit Load List:'):
+  my_cnx = sf.connect(**st.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  st.header("The fruit load list contains:")
+  st.dataframe(my_data_rows)
 
-
-my_cnx = sf.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-fruit_add = streamlit.text_input('What fruit would you like add?')
-streamlit.text('Thanks for adding ' + fruit_add)
+fruit_add = st.text_input('What fruit would you like add?')
+st.text('Thanks for adding ' + fruit_add)
 
