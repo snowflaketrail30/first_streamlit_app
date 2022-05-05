422b2906c12f47f6a1c96ed414a71c4e3c0ee55e
Haritha Sabbisetty <104927752+snowflaketrail30@users.noreply.github.com>
2022-05-05 17:50:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,8 +40,9 @@ try:
     st.dataframe(fruityvice_normalized)
 except URLError as e:
   st.error()
-  
-if sf.button('Get Fruit Load List:'):
+
+#Add a button to load list  
+if st.button('Get Fruit Load List:'):
   my_cnx = sf.connect(**st.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   st.header("The fruit load list contains:")
